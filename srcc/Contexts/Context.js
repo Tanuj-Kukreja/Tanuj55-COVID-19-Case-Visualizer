4cde7f9b8c63934aa3fe8056bf7ac43f2f1b7ec5
Tanuj55 <52146843+Tanuj55@users.noreply.github.com>
2021-03-01 16:28:36 +0530

    Delete srcc directory

--- srcc/Contexts/Context.js
+++
@@ -1,7 +0,0 @@
-import  { createContext } from 'react'
-
-const Context  = createContext({
-    click:null
-})
-
-export default Context
--- srcc/components/Search bar/SearchBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const SearchBar = props=>(
-    <input onChange={props.change} placeholder='Enter the name of a country'></input>
-)
-
-export default SearchBar
